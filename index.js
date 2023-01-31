85aea0294438be7fa20ed9a403a5f8c14b114ad6
Vikash <vksingh2801@gmail.com>
2023-01-31 11:51:59 +0530

    updated live paymentapi host

--- index.js
+++ index.js
@@ -41,7 +41,8 @@ if(!paymentDetails.amount || !paymentDetails.customerId || !paymentDetails.custo
     params['CUST_ID'] = paymentDetails.customerId;
     params['TXN_AMOUNT'] = paymentDetails.amount;
     /* where is app is hosted (heroku url)*/
-    params['CALLBACK_URL'] = 'http://localhost:4100/callback';
+    //params['CALLBACK_URL'] = 'http://localhost:4100/callback'; 
+	params['CALLBACK_URL'] = 'https://paymentapi-v4t6.onrender.com/callback';
     params['EMAIL'] = paymentDetails.customerEmail;
     params['MOBILE_NO'] = paymentDetails.customerPhone;
   
